0dc12b5750dc3103924a5c681cc768e38baea2e6
kralonur <kralonur1998@gmail.com>
2024-01-27 17:30:05 +0300

    feat: ability to switch public minting

--- contracts/GLNTToken.sol
+++ contracts/GLNTToken.sol
@@ -6,37 +6,35 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract GLNTToken is ERC721, ERC721URIStorage, AccessControl {
-    // TODO: this contract is not ready, change later when decisions are made
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    bool public isPublicMintingEnabled;
+
     uint256 private _nextTokenId;
 
     constructor() ERC721("GLNTToken", "GLNT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
+    function switchPublicMinting(bool enabled) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        isPublicMintingEnabled = enabled;
+    }
+
+    function safeMint(address to, string memory uri) external {
+        if (!isPublicMintingEnabled) _checkRole(MINTER_ROLE);
+
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage, AccessControl)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage, AccessControl) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
